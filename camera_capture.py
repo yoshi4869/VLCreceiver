b478244236f7dd72916af9e12b4bc035dbbaf0cc
yoshi4869 <yoshihito4869@gmail.com>
2020-04-16 11:10:23 +0900

    WEBカメラの動画を定期的に画像として自動保存する

--- camera_capture.py
+++ camera_capture.py
@@ -1,29 +1,33 @@
 import cv2
 import os
+import datetime
+#参考
 #カメラからの映像をリアルタイムで表示し、
 #キーボードを押下したタイミングのフレームを静止画の画像ファイルとして切り出して保存するサンプルコード。
 # https://note.nkmk.me/python-opencv-camera-to-still-image/
-def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
-    cap = cv2.VideoCapture(device_num)#引数でカメラを選べる 0:PC内蔵カメラ, 1:USB Webカメラ
+def save_frame_camera_cycle(device_num, dir_path, basename, cycle, ext='jpg', delay=1, window_name='frame'):
+    cap = cv2.VideoCapture(device_num)
 
     if not cap.isOpened():
         return
 
-    os.makedirs(dir_path, exist_ok=True)#ディレクトリの作成
-    base_path = os.path.join(dir_path, basename)#パスを結合
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
 
     n = 0
     while True:
         ret, frame = cap.read()
         cv2.imshow(window_name, frame)
-        key = cv2.waitKey(delay) & 0xFF
-        if key == ord('c'):#キーボードのcを押すと指定したディレクトリに<basename>_<連番>.<拡張子>というファイル名で静止画が保存される。
-            cv2.imwrite('{}_{}.{}'.format(base_path, n, ext), frame)#番号を末尾につける
-            n += 1
-        elif key == ord('q'):#キーボードのqを押すと終了
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
             break
+        if n == cycle:
+            n = 0
+            cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
+        n += 1
 
     cv2.destroyWindow(window_name)
 
-
-save_frame_camera_key(1, 'data/temp', 'camera_capture')
+#OpenCVによって表示されるフレームをカウントしているためカメラのFPS設定ではなく表示される映像のFPSが基準となる。
+# 30FPS程度で表示されるのであればcycle=300とすると10秒おきに画像が保存される。
+#保存するフレームの周期を指定する変数cycleを小さい値にすると大量の画像が保存されてしまうので注意。
+save_frame_camera_cycle(1, 'data/temp', 'camera_capture_cycle', 30)
